8b9454b5ee359f3bddeb5a98740b166d1b60bcea
zoefisk <zoejewelfisk@gmail.com>
2026-05-04 23:00:45 -0400

    temporarily disable light mode until colors are improved

--- app/components/ThemeToggle.tsx
+++ app/components/ThemeToggle.tsx
@@ -26,30 +26,35 @@ export function ThemeToggle() {
   const nextTheme = theme === "light" ? "dark" : "light";
 
   return (
-    <button
-      aria-label="Toggle color mode"
-      className="theme-toggle"
-      onClick={() => {
-        const updatedTheme = nextTheme;
-        setTheme(updatedTheme);
-        applyTheme(updatedTheme);
-      }}
-      title="Toggle color mode"
-      type="button"
-    >
-      <span className="sr-only">Toggle color mode</span>
-      <span className="theme-toggle-icon" aria-hidden="true">
-        <svg fill="none" viewBox="0 0 24 24">
-          <path
-            d="M12 3.5v2.2M12 18.3v2.2M5.99 5.99l1.56 1.56M16.45 16.45l1.56 1.56M3.5 12h2.2M18.3 12h2.2M5.99 18.01l1.56-1.56M16.45 7.55l1.56-1.56"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeWidth="1.5"
-          />
-          <circle cx="12" cy="12" fill="currentColor" r="3.2" />
-        </svg>
-      </span>
-      <span className="theme-toggle-text">{theme === "light" ? "Dark" : "Light"}</span>
-    </button>
-  );
+      <>
+      </>
+  )
+
+  // return (
+  //   <button
+  //     aria-label="Toggle color mode"
+  //     className="theme-toggle"
+  //     onClick={() => {
+  //       const updatedTheme = nextTheme;
+  //       setTheme(updatedTheme);
+  //       applyTheme(updatedTheme);
+  //     }}
+  //     title="Toggle color mode"
+  //     type="button"
+  //   >
+  //     <span className="sr-only">Toggle color mode</span>
+  //     <span className="theme-toggle-icon" aria-hidden="true">
+  //       <svg fill="none" viewBox="0 0 24 24">
+  //         <path
+  //           d="M12 3.5v2.2M12 18.3v2.2M5.99 5.99l1.56 1.56M16.45 16.45l1.56 1.56M3.5 12h2.2M18.3 12h2.2M5.99 18.01l1.56-1.56M16.45 7.55l1.56-1.56"
+  //           stroke="currentColor"
+  //           strokeLinecap="round"
+  //           strokeWidth="1.5"
+  //         />
+  //         <circle cx="12" cy="12" fill="currentColor" r="3.2" />
+  //       </svg>
+  //     </span>
+  //     <span className="theme-toggle-text">{theme === "light" ? "Dark" : "Light"}</span>
+  //   </button>
+  // );
 }
